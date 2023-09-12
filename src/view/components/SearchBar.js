e568d3b6f67a2954843137cb59c7e28b6761797c
TessaNyquist <tessanyquist@outlook.com>
2023-09-11 20:26:05 -0400

    start adding addditional fucntionality

--- src/view/components/SearchBar.js
+++ src/view/components/SearchBar.js
@@ -1,4 +1,5 @@
 import react, { useState } from "react";
+import axios from "axios";
 
 function SearchBar({onSearch}) {
     const [searchTerm, setSearchTerm] = useState('');
@@ -7,7 +8,17 @@ function SearchBar({onSearch}) {
         setSearchTerm(e.target.value);
     };
     const handleSearch = () => {
-        onSearch(searchTerm);
+        //onSearch(searchTerm);
+        try {
+            // Send an HTTP POST request to your backend API endpoint
+            const response = await axios.post('/api/search', { query: searchTerm });
+
+            // Handle the response data (results) from the server
+            onSearch(response.data.results);
+        } catch (error) {
+            console.error('Error searching:', error);
+            // Handle errors, e.g., show an error message to the user
+        }
     };
 
     return (
